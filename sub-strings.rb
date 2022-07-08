23e9c562745bec41226d36959982451390f35653
Frobi <frobi.webdev@outlook.com>
2022-07-08 12:24:23 +0930

    create-method-which-checks-for-each-word-once

--- sub-strings.rb
+++ sub-strings.rb
@@ -1,1 +1,18 @@
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
+def substrings(string, dictionary)
+    
+    found_words = Hash.new(0)
+
+    dictionary.each do |word| 
+        if string.downcase.include?(word)
+            found_words[word] += 1
+            p found_words
+        end
+    end
+
+    return found_words
+
+end
+
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
